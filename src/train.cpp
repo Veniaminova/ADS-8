7b547e400d287f330944b8c1df36d15823b48ced
Анастасия <98629650+Veniaminova@users.noreply.github.com>
2024-05-01 17:25:09 +0300

    train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,37 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+Train::Train() :first(nullptr) {}
+
+void Train::addCage(bool light) {
+    Cage* newCage = new Cage();
+    newCage->light = light;
+    if (first == nullptr) {
+        newCage->next = newCage;
+        newCage->prev = newCage;
+        first = newCage;
+    } else {
+        Cage* lastCage = first->prev;
+        newCage->next = first;
+        newCage->prev = lastCage;
+        first->prev = newCage;
+        lastCage->next = newCage;
+    }
+}
+
+int Train::getLength() {
+    int length = 0;
+    if (first != nullptr) {
+        length = 1;
+        Cage* currentCage = first->next;
+        while (currentCage != first) {
+            length++;
+            currentCage = currentCage->next;
+        }
+    }
+    countOp = length;
+    return countOp;
+}
+
+int Train::getOpCount() {
+    return countOp;
+}
